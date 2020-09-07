c38efd14dfc113ff79e18cb14a1a5a949cbca2cc
Jean M. Cyr <jean.m.cyr@gmail.com>
2020-09-07 17:14:05 -0400

    CUDA style

--- tatami.cu
+++ tatami.cu
@@ -1,7 +1,6 @@
-// tatami.cu (simplified single kernel)
+// tatami.cu (J.M.Cyr 2020)
 
 #include <cuda.h>
-
 #include <iostream>
 
 const unsigned nMax(100000000);
@@ -9,45 +8,47 @@ const unsigned nMaxSqrt(sqrt(nMax));
 
 __global__ void count(unsigned* v, unsigned base)
 {
-    const unsigned i = threadIdx.x + base + 7;
-    const unsigned loop_increment = (i & 1) + 1;
-    unsigned k2 = i + 3;
-    unsigned k3 = i + i - 4;
-    while ((i * k2 < nMax) && (k2 <= k3))
+    const unsigned i(threadIdx.x + base);
+    const unsigned loopIncrement((i & 1) + 1);
+    unsigned k2(i + 3);
+    unsigned k3(i + i - 4);
+    unsigned k5(i * k2);
+    while ((k5 < nMax) && (k2 <= k3))
     {
-        const unsigned k4 = min((nMax - 1) / i, k3);
-        for (unsigned j = k2; j <= k4; j += loop_increment)
+        const unsigned k4(min((nMax - 1) / i, k3));
+        for (unsigned j(k2); j <= k4; j += loopIncrement)
             atomicAdd(v + i * j, 1);
-        k2 += i + 1;
         k3 += i - 1;
+        k5 += (k2 += i + 1);
     }
 }
 
 unsigned Tatami(unsigned s)
 {
+    const unsigned sizeV(sizeof(unsigned) * nMax);
+    const unsigned groupSize(256);
     unsigned* v;
 
-    cudaMalloc(&v, sizeof(unsigned) * nMax);
-    cudaMemset(v, 0, sizeof(unsigned) * nMax);
-    const unsigned max_group_size = 1024;
+    cudaMalloc(&v, sizeV);
+    cudaMemset(v, 0, sizeV);
 
-    unsigned iterations = nMaxSqrt - 7;
-    for (unsigned iteration = 0; iteration < iterations;
-         iteration += max_group_size)
-        count<<<1, min(iterations - iteration, max_group_size)>>>(v, iteration);
+    for (unsigned i(7); i < nMaxSqrt; i += groupSize)
+        count<<<1, min(nMaxSqrt - i, groupSize)>>>(v, i);
 
     unsigned* vh = new unsigned[nMax];
-    cudaMemcpy(vh, v, sizeof(unsigned) * nMax, cudaMemcpyDeviceToHost);
+    cudaMemcpy(vh, v, sizeV, cudaMemcpyDeviceToHost);
 
-    for (unsigned i = 7; i < nMax; i++)
+    for (unsigned i(7); i < nMax; i++)
         if (vh[i] == s)
             return i;
 
-    return 0;  // shouldn't happen
+    abort();  // shouldn't happen
 }
 
-int main()
+int main(int ac, char** av)
 {
-    const unsigned s = 200;
+    unsigned s(200);
+    if (ac >= 2)
+        s = atoi(av[1]);
     std::cout << "T(" << Tatami(s) << ")=" << s << std::endl;
 }
